e6afe4056d23a3ec1ee28dd15479ca940dd98e36
Maxim Kravchuk <54956796+MaxKravchuk@users.noreply.github.com>
2023-02-24 19:12:04 +0100

    added New folder logic

--- FileSystem/Controllers/HomeController.cs
+++ FileSystem/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
-﻿using FileSystem.Models;
+﻿using FileSystem.Context;
+using FileSystem.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
 
 namespace FileSystem.Controllers
@@ -7,14 +9,38 @@ namespace FileSystem.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly DataContext context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, DataContext _context)
         {
             _logger = logger;
+            context = _context;
         }
 
-        public IActionResult Index()
+        public IActionResult Index(int? id = null)
         {
+            var folders = context.Folders.Where(x => x.Parent_Id == id);
+            ViewBag.Id = id;
+            return View(folders);
+        }
+
+        [HttpGet]
+        public IActionResult Create(int? id = null)
+        {
+            ViewBag.ParrentId = id;
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Folder folder)
+        {
+            if (ModelState.IsValid)
+            {   
+                if(folder.Id!=0) folder.Id = 0;
+                context.Folders.Add(folder);
+                context.SaveChanges();
+                return RedirectToAction("Index");
+            }
             return View();
         }
 
