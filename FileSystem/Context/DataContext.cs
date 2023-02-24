c8ff3470da53d4486df8e9755ae66cab0efeff32
Maxim Kravchuk <54956796+MaxKravchuk@users.noreply.github.com>
2023-02-24 16:09:45 +0100

    Add project files.

---
+++ FileSystem/Context/DataContext.cs
@@ -0,0 +1,17 @@
+﻿using FileSystem.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FileSystem.Context
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> dbContextOptions) : base(dbContextOptions) { }
+        DbSet<Folder> Folders { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Folder>().ToTable("Catalog");
+        }
+
+    }
+}
---
+++ FileSystem/Models/Folder.cs
@@ -0,0 +1,9 @@
+﻿namespace FileSystem.Models
+{
+    public class Folder
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public int? Parent_Id { get; set; }
+    }
+}
